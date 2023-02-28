1b866e09e291b7cce4878fa12c4b63cc25a7875f
Michael DesRoches <mdesroches@nevada.unr.edu>
2023-02-28 15:09:16 -0800

    storage test

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import ErrorPage from "./pages/Error"
 import ProjectsPage from "./pages/Projects"
 import BlogPage from "./pages/Blog"
 import TutorialsPage from "./pages/Tutorials"
+import { Amplify, Storage } from "aws-amplify"
+import awsconfig from "./aws-exports"
+Amplify.configure(awsconfig)
 
 const router = createBrowserRouter([
   {
