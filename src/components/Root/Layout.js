76cadedcea4200f3a0a9038c00c3cc37909a58d2
Michael DesRoches <mdesroches@nevada.unr.edu>
2023-02-28 17:17:14 -0800

    removed aws config

--- src/components/Root/Layout.js
+++ src/components/Root/Layout.js
@@ -2,9 +2,6 @@ import React from "react"
 import Footer from "./Footer"
 import Header from "./Header"
 import styles from "./Layout.module.css"
-import { Amplify } from "aws-amplify"
-import awsconfig from "../../aws-exports"
-Amplify.configure(awsconfig)
 
 const Layout = ({ children }) => {
   return (
