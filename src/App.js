3615a10b8f68e113570f2348665f404e9ce763b6
Michael DesRoches <mdesroches@nevada.unr.edu>
2023-02-28 19:01:35 -0800

    added custon build

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import ProjectsPage from "./pages/Projects"
 import BlogPage from "./pages/Blog"
 import TutorialsPage from "./pages/Tutorials"
 import { Amplify, Storage } from "aws-amplify"
-import awsconfig from "../src/aws-exports"
+import awsconfig from "./aws-exports.js"
 Amplify.configure(awsconfig)
 
 const router = createBrowserRouter([
--- src/components/Home/Introduction.js
+++ src/components/Home/Introduction.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React from "react"
 import styles from "./Introduction.module.css"
 import Button from "./Button"
 import resume from "../../images/MikesResume.pdf"
