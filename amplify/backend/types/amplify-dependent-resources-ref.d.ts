fc87a4faec9366853bd6a1e3863538765d3784f6
Michael DesRoches <mdesroches@nevada.unr.edu>
2023-02-28 17:49:38 -0800

    Added amplify config

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -9,5 +9,11 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolId": "string",
       "UserPoolName": "string"
     }
+  },
+  "storage": {
+    "s3508a7670": {
+      "BucketName": "string",
+      "Region": "string"
+    }
   }
 }
